a27996500076837a259ba2a6c7ebeabcbcff8c50
Shinobu KINJO <kinjo@sensetime.com>
2020-11-04 07:05:46 +0900

    modified:   shared_optimizer.py

--- shared_optimizer.py
+++ shared_optimizer.py
@@ -47,6 +47,59 @@ class SharedAdam(Adam):
                     p.data.addcdiv_(-step_size, exp_avg, denom)
         return loss
 
+class SharedLRSchedulerAdam(Adam):
+    def __init__(self, params, max_step, lr = 1e-3, betas = (0.9, 0.999),
+            eps = 1e-3, weight_decay = 0.0, lrs = [1e-4, 1e-4 * 0.9, 1e-4 * 0.8,
+                1e-4 * 0.7, 1e-4 * 0.6, 1e-4 * 0.5, 1e-4 * 0.4, 1e-4 * 0.3,
+                1e-4 * 0.2, 1e-4 * 0.1], num_steps = 10):
+        super(SharedLRSchedulerAdam, self).__init__(params, lr, betas, eps,
+                weight_decay)
+        self.max_step = max_step
+        self.lrs = lrs
+        self.num_steps = num_steps
+        for group in self.param_groups:
+            for p in group['params']:
+                state = self.state[p]
+                state['step'] = torch.zeros(1)
+                state['exp_avg'] = p.data.new().resize_as_(p.data).zero_()
+                state['exp_avg_sq'] = p.data.new().resize_as_(p.data).zero_()
+
+    def share_memory(self):
+        for group in self.param_groups:
+            for p in group['params']:
+                state = self.state[p]
+                state['step'].share_memory_()
+                state['exp_avg'].share_memory_()
+                state['exp_avg_sq'].share_memory_()
+
+    def step(self, closure = None):
+        loss = None
+        if closure is not None:
+            loss = closure()
+        lr = self.lrs[int(state['step'][0] //
+            (self.max_step // self.num_steps))]
+        group['lr'] = lr
+        for group in self.param_groups:
+            for p in group['params']:
+                if p.grad is None:
+                    continue
+                grad = p.grad.data
+                state = self.state[p]
+                exp_avg = state['exp_avg']
+                exp_avg_sq = state['exp_avg_sq']
+                beta1, beta2 = group['betas']
+                state['step'] += 1
+                if group['weight_decay'] != 0:
+                    grad = grad.add(group['weight_decay'], p.data)
+                exp_avg.mul_(beta1).add_(1 - beta1, grad)
+                exp_avg_sq.mul_(beta2).addcmul_(1 - beta2, grad, grad)
+                denom = exp_avg_sq.sqrt().add_(group['eps'])
+                bias_correction1 = 1 - beta1 ** state['step'][0]
+                bias_correction2 = 1 - beta2 ** state['step'][0]
+                step_size = group['lr'] * math.sqrt(bias_correction2) / \
+                        bias_correction1
+                p.data.addcdiv_(-step_size, exp_avg, denom)
+
 class SharedRMSprop(RMSprop):
     def __init__(self, params, lr = 1e-3, alpha = 0.99, eps = 1e-8,
             weight_decay = 0.0, momentum = 0.0, centered = False):
